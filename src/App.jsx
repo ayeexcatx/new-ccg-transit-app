cb57e41d185e834a76a3127c654689202b7748b8
ayeexcatx <acatarino777@gmail.com>
2026-03-09 23:36:37 -0400

    Add global scroll reset on pathname navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import NavigationTracker from '@/lib/NavigationTracker'
+import ScrollToTop from '@/lib/ScrollToTop'
 import { pagesConfig } from './pages.config'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
@@ -73,6 +74,7 @@ function App() {
       <QueryClientProvider client={queryClientInstance}>
         <Router>
           <NavigationTracker />
+          <ScrollToTop />
           <InstallPromptBanner />
           <NewVersionBanner />
           <AuthenticatedApp />

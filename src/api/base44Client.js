605505fd6181fca10f9cae25c8084b3167a36c68
ayeexcatx <acatarino777@gmail.com>
2026-03-09 22:52:10 -0400

    Update base44Client.js

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -3,7 +3,7 @@ import { appParams } from '@/lib/app-params';
 
 const { appId, token, functionsVersion, appBaseUrl } = appParams;
 
-//Create a client with authentication required ddd
+//Create a client with authentication required
 export const base44 = createClient({
   appId,
   token,
